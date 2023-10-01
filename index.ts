577e9ef76fd2f7d4b3cea6d691ccd7d298ea970b
osawa-koki <koki@osawa.dev>
2023-10-01 19:39:51 +0900

    基数ソートの実装。

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+import shuffle from './src/shuffle'
+import radixSort from './src/radixSort'
+
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+shuffle(array)
+
+console.log(`Shuffled array: ${array.join(', ')}`)
+
+radixSort(array)
+
+console.log(`Sorted array: ${array.join(', ')}`)
